a30610c9f484950d316205a9253a31363ca1f07d
Nikolay Arefev <nikolare@hplt-compute.nird.hplt-ns8112k.svc.cluster.local>
2024-10-09 11:47:00 +0200

    command line arguments: avoid file pattern expansion

--- run_retrieval1.sh
+++ run_retrieval1.sh
@@ -5,5 +5,7 @@ OUTD=$3
 TEXTFIELD=$4  # 'text', 't' for outputs from stage2
 FILEPATTERN=$5 # '*.zst', 'text.zst' for outputs from stage2
 
+echo "Running retrieval:" "$FILEPATTERN"
+
 mkdir -p $OUTD
-find $HPLT_DIR -name $FILEPATTERN | parallel --eta --joblog $OUTD/joblog --linebuffer -j 100 "python -u retriever.py retrieve {} -t $TEXTFIELD --fcontent=$PKL >$OUTD/{#}.out 2>$OUTD/{#}.err"
+find $HPLT_DIR -name "$FILEPATTERN" | parallel --eta --joblog $OUTD/joblog --linebuffer -j 100 "python -u retriever.py retrieve {} -t $TEXTFIELD --fcontent=$PKL >$OUTD/{#}.out 2>$OUTD/{#}.err"
